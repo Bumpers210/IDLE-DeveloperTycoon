93f825723cd6a6fc5a609ba1f5e49fddad5c0c04
Bumpers210 <jbuchner89@googlemail.com>
2024-05-31 19:36:59 +0200

    Changes to be committed: 	modified:   developer_manager.py 	modified:   gui.py 	modified:   main.py 	modified:   models.py

--- developer_manager.py
+++ developer_manager.py
@@ -1,16 +1,52 @@
 import random
 from tkinter import messagebox
 from models import Developer
-
+import tkinter as tk
 
 class DeveloperManager:
     def __init__(self, game):
         self.game = game
 
-    def create_new_developer(self):
+    def create_new_developer_start(self):
         print("Creating new developer...")
-        Developer.create_new_developer(self.game.developers)
+        Developer.create_new_developer_start(self.game.developers)
         print("New developer created.")
+
+    def open_hire_developer_selection(self):
+        hire_window = tk.Toplevel(self.game.gui.root)
+        hire_window.title("Hire Developer")
+
+        def hire_beginner_developer():
+            self.hire_developer("Beginner")
+            hire_window.destroy()
+
+        def hire_advanced_developer():
+            self.hire_developer("Advanced")
+            hire_window.destroy()
+
+        def hire_professional_developer():
+            self.hire_developer("Professional")
+            hire_window.destroy()
+
+        tk.Button(hire_window, text="Beginner Developer", command=hire_beginner_developer).pack(pady=10)
+        tk.Button(hire_window, text="Advanced Developer", command=hire_advanced_developer).pack(pady=10)
+        tk.Button(hire_window, text="Professional Developer", command=hire_professional_developer).pack(pady=10)
+
+    def hire_developer(self, type):
+        if type == "Beginner":
+            new_developer = Developer.create_new_developer_beginner(self.game.developers)
+        elif type == "Advanced":
+            new_developer = Developer.create_new_developer_advanced(self.game.developers)
+        elif type == "Professional":
+            new_developer = Developer.create_new_developer_professional(self.game.developers)
+        
+        if new_developer:
+            self.game.developers.append(new_developer)
+            self.game.update_income_per_minute()
+            self.game.gui.update_gui()
+            print(f"{type} developer hired.")
+        else:
+            print(f"Failed to hire {type} developer.")
 
     def train_developer(self):
         if self.game.developers:
